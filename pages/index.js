beaa2468b39bd3e907a9390200ffdb8246daacec
samuznz <164966123+samuznz@users.noreply.github.com>
2024-03-25 22:52:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Samuzn.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my site!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          My name is Samuel! and I'm starting programming.<code>pages/index.js</code>
         </p>
       </main>
 
